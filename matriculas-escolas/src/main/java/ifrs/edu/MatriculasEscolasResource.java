1ab0c021971790035151c2a820e1547aceb65f18
amimenezes <mi.menezes.luiz@gmail.com>
2022-06-09 10:29:36 -0300

    WIP: Mudança de POST pra GET

--- matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasResource.java
+++ matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasResource.java
@@ -1,24 +1,35 @@
 package ifrs.edu;
 
-import javax.inject.Singleton;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 @Path("/matriculas-escolas")
-@Singleton
 public class MatriculasEscolasResource {
     
-    @POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    // @POST
+    // @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    // @Produces(MediaType.APPLICATION_JSON)
+    // public MatriculaEscola setIdMatriculasEscolas(@FormParam("id") Integer id, String nome) {
+    //     MatriculaEscola m = new MatriculaEscola();
+    //     m.setId(id);
+    //     m.setName(nome);
+    //     return m;
+    // }
+    
+    @Inject
+    @RestClient
+    MatriculasEscolasService matriculasEscolasService;
+    
+    @GET
+    @Path("/findByCodigo/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public MatriculaEscola setIdMatriculasEscolas(@FormParam("id") Integer id, String nome) {
-        MatriculaEscola m = new MatriculaEscola();
-        m.setId(id);
-        m.setName(nome);
-        return m;
+    public MatriculaEscola getCodigoMatriculasEscolas(@QueryParam("id") Integer id) {
+        return matriculasEscolasService.findById(id);
     }
 }
--- matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasService.java
+++ matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasService.java
@@ -1,22 +1,26 @@
 package ifrs.edu;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-@Path("/action/datastore_search?")
 @RegisterRestClient
 public interface MatriculasEscolasService {
     
-    @POST
+    // @POST
+    // @Produces(MediaType.APPLICATION_JSON)
+    // @Consumes(MediaType.MULTIPART_FORM_DATA)
+    // @Path("resource_id=5b5bc272-6666-4d24-8ddd-138fea511809")
+    // public MatriculaEscola findById(@PathParam("_id") Integer id);
+    // public MatriculaEscola findByName(@PathParam("nome") String nome);
+    //@Path("/action/datastore_search?resource_id=5b5bc272-6666-4d24-8ddd-138fea511809&filters={\"_id\":\"id\"}")
+    @GET
+    @Path("/action/datastore_search?resource_id=5b5bc272-6666-4d24-8ddd-138fea511809&fields")
     @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    @Path("resource_id=5b5bc272-6666-4d24-8ddd-138fea511809")
-    public MatriculaEscola findById(@PathParam("_id") Integer id);
-    public MatriculaEscola findByName(@PathParam("nome") String nome);
+    public MatriculaEscola findById(@QueryParam("_id") Integer id);
+    
 }
