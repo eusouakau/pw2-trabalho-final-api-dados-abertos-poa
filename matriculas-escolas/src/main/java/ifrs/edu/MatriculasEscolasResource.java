5c991a1d1cdd8b72878b8ba76e935b4cb060f20c
amimenezes <mi.menezes.luiz@gmail.com>
2022-06-09 11:59:46 -0300

    WIP: Mudança de atributo na URL para Name

--- matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasResource.java
+++ matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasResource.java
@@ -1,35 +1,44 @@
 package ifrs.edu;
 
+import java.util.ArrayList;
+
 import javax.inject.Inject;
+import javax.swing.JOptionPane;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/matriculas-escolas")
 public class MatriculasEscolasResource {
-    
-    // @POST
-    // @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-    // @Produces(MediaType.APPLICATION_JSON)
-    // public MatriculaEscola setIdMatriculasEscolas(@FormParam("id") Integer id, String nome) {
-    //     MatriculaEscola m = new MatriculaEscola();
-    //     m.setId(id);
-    //     m.setName(nome);
-    //     return m;
-    // }
-    
+   
+    //private static Integer _id = 5;
+    static boolean flag = false;
+
+    //private static String name;
+
     @Inject
     @RestClient
     MatriculasEscolasService matriculasEscolasService;
     
     @GET
-    @Path("/findByCodigo/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public MatriculaEscola getCodigoMatriculasEscolas(@QueryParam("id") Integer id) {
-        return matriculasEscolasService.findById(id);
+    @Path("/findByEscolas/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getString(@PathParam("name") String name){
+        return name;
     }
+    // public static void findById(ArrayList<MatriculaEscola> listaMatriculasEscolas) {
+    //     //return matriculasEscolasService.findById(id);
+    //     for (MatriculaEscola escolaId : listaMatriculasEscolas) {
+    //         if (escolaId.getName().equals(name)) {
+    //             JOptionPane.showMessageDialog(null, "Escola localizada: " + escolaId.toString());
+    //             flag = true;
+    //         }
+    //     }
+    //     if (!flag)
+	// 			JOptionPane.showMessageDialog(null, "Escola inexistente!");
+    // }
 }
--- matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasService.java
+++ matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasService.java
@@ -2,8 +2,8 @@ package ifrs.edu;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
@@ -11,16 +11,9 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 @RegisterRestClient
 public interface MatriculasEscolasService {
     
-    // @POST
-    // @Produces(MediaType.APPLICATION_JSON)
-    // @Consumes(MediaType.MULTIPART_FORM_DATA)
-    // @Path("resource_id=5b5bc272-6666-4d24-8ddd-138fea511809")
-    // public MatriculaEscola findById(@PathParam("_id") Integer id);
-    // public MatriculaEscola findByName(@PathParam("nome") String nome);
-    //@Path("/action/datastore_search?resource_id=5b5bc272-6666-4d24-8ddd-138fea511809&filters={\"_id\":\"id\"}")
     @GET
-    @Path("/action/datastore_search?resource_id=5b5bc272-6666-4d24-8ddd-138fea511809&fields")
+    @Path("/action/datastore_search?resource_id=5b5bc272-6666-4d24-8ddd-138fea511809&q={name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public MatriculaEscola findById(@QueryParam("_id") Integer id);
-    
+    public MatriculaEscola findById(@PathParam("name") String name);
+
 }
