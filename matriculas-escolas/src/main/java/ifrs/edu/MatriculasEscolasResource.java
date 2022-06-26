42f56f849eac4c8499ce3860d75f5d9ad7270061
amimenezes <mi.menezes.luiz@gmail.com>
2022-06-25 23:19:26 -0300

    WIP: puxa todos as Matriculas de Escolas com getAll

--- matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasResource.java
+++ matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasResource.java
@@ -6,26 +6,30 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import io.vertx.core.json.JsonObject;
 
 @Path("/matriculas-escolas")
+@Produces(MediaType.APPLICATION_JSON)
 public class MatriculasEscolasResource {
-   
-    //private static Integer _id = 5;
-    static boolean flag = false;
-
-    //private static String name;
+    
+    static String resource_id = "5b5bc272-6666-4d24-8ddd-138fea511809";
+    //static boolean flag = false;
 
     @Inject
     @RestClient
     MatriculasEscolasService matriculasEscolasService;
     
     @GET
-    @Path("/findByEscolas/{name}")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getString(@PathParam("name") String name){
-        return name;
+    @Path("/findById/{id}")
+    public String getMatrEscById(@PathParam("id") String id){
+        return matriculasEscolasService.findById(resource_id, "102");
+    }
+
+    @GET
+    @Path("/all")
+    public String getAllMatrEsc(){
+        return matriculasEscolasService.getAll();
     }
     // public static void findById(ArrayList<MatriculaEscola> listaMatriculasEscolas) {
     //     //return matriculasEscolasService.findById(id);
