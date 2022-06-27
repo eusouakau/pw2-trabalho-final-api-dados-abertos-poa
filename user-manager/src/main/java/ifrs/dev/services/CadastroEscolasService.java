eea1233f1b2c11b8c91980694acbf585b9ac8e7f
amimenezes <mi.menezes.luiz@gmail.com>
2022-06-27 00:18:51 -0300

    FEAT: Get de Filters funcionando!

--- user-manager/src/main/java/ifrs/dev/services/CadastroEscolasService.java
+++ user-manager/src/main/java/ifrs/dev/services/CadastroEscolasService.java
@@ -1,9 +1,9 @@
 package ifrs.dev.services;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+// import javax.ws.rs.GET;
+// import javax.ws.rs.Path;
+// import javax.ws.rs.Produces;
+// import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
