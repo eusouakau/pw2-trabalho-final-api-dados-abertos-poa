32571c1c11e790011166c748f2c214372568748a
Kauana Henriques <karoltvbl@gmail.com>
2022-07-06 00:46:01 -0300

    fix: (user) remove import desnecessário

--- user-manager/src/main/java/ifrs/dev/services/ServidoresAtivosService.java
+++ user-manager/src/main/java/ifrs/dev/services/ServidoresAtivosService.java
@@ -3,7 +3,6 @@ package ifrs.dev.services;
 import java.util.List;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
