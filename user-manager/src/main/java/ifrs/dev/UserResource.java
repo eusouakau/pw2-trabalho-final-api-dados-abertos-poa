9a444e9a69374759b225f14dce30a4bcb63a9608
Felipe Pizzio <felipetpizzio@gmail.com>
2022-07-06 01:10:48 -0300

    add integração user + cadastro-escola

--- user-manager/src/main/java/ifrs/dev/UserResource.java
+++ user-manager/src/main/java/ifrs/dev/UserResource.java
@@ -13,72 +13,106 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 import ifrs.dev.models.User;
+import ifrs.dev.services.CadastroEscolasService;
 import ifrs.dev.services.UserService;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
-    
-    @Inject
-    UserService userService;
-    
-    @GET
-    public Response getAllUsers() {
-        try{
-            return Response.status(Status.OK).entity(userService.getAllUsers()).build();
-        } catch (Exception e) {
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
-    }
 
-    @GET
-    @Path("/{name}")
-    public Response getUserByName(@PathParam("name") String name) {
-        try{
-            return Response.status(Status.OK).entity(userService.getUserByName(name)).build();
-        } catch (Exception e) {
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
-    }
-    
-    @POST
-    public Response createUser(User user) {
-        try{
-            return Response.status(Status.CREATED).entity(userService.createUser(user)).build();
-        } catch (Exception e) {
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
-    }
+  @Inject
+  @RestClient
+  CadastroEscolasService cadastroEscolasService;
 
-    @POST
-    @Path("/login")
-    public Response login(User user) {
-        try{
-            return Response.status(Status.OK).entity(userService.login(user)).build();
-        } catch (Exception e) {
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
-    }
+  @Inject
+  UserService userService;
 
-    @PUT
-    @Path("/atualizar/{id}")
-    public Response updateUser(@PathParam("id") Long id, User user) {
-        try{
-            return Response.status(Status.OK).entity(userService.updateUser(user)).build();
-        } catch (Exception e) {
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
+  @GET
+  public Response getAllUsers() {
+    try {
+      return Response.status(Status.OK).entity(userService.getAllUsers()).build();
+    } catch (Exception e) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
     }
+  }
 
-    @DELETE
-    @Path("/deletar/{id}")
-    public Response deleteUser(@PathParam("id") Long id) {
-        try{
-            return Response.status(Status.OK).entity(userService.deleteUser(id)).build();
-        } catch (Exception e) {
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
-        }
+  @GET
+  @Path("/{name}")
+  public Response getUserByName(@PathParam("name") String name) {
+    try {
+      return Response.status(Status.OK).entity(userService.getUserByName(name)).build();
+    } catch (Exception e) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
     }
+  }
+
+  @POST
+  public Response createUser(User user) {
+    try {
+      return Response.status(Status.CREATED).entity(userService.createUser(user)).build();
+    } catch (Exception e) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+    }
+  }
+
+  @POST
+  @Path("/login")
+  public Response login(User user) {
+    try {
+      return Response.status(Status.OK).entity(userService.login(user)).build();
+    } catch (Exception e) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+    }
+  }
+
+  @PUT
+  @Path("/atualizar/{id}")
+  public Response updateUser(@PathParam("id") Long id, User user) {
+    try {
+      return Response.status(Status.OK).entity(userService.updateUser(user)).build();
+    } catch (Exception e) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+    }
+  }
+
+  @DELETE
+  @Path("/deletar/{id}")
+  public Response deleteUser(@PathParam("id") Long id) {
+    try {
+      return Response.status(Status.OK).entity(userService.deleteUser(id)).build();
+    } catch (Exception e) {
+      return Response.status(Status.INTERNAL_SERVER_ERROR).entity(e.getMessage()).build();
+    }
+  }
+
+  @GET
+  @Path("/escolas/lista-todos-cadastros-escolas")
+  public JsonArray getAllCE() {
+    return cadastroEscolasService.getAllCE();
+  }
+
+  @GET
+  @Path("/escolas/codigo/{_codigo}")
+  public JsonObject getCEByCodigo(@PathParam("_codigo") Integer _codigo) {
+    return cadastroEscolasService.getCEByCodigo(_codigo);
+  }
+
+  @GET
+  @Path("/escolas/quantidade/")
+  public String getQuantidadeByDepAdm() {
+    return cadastroEscolasService.getQuantidadeByDepAdm();
+  }
+
+  @GET
+  @Path("/escolas/nome/{_name}")
+  public JsonArray findByName(@PathParam("_name") String _name) {
+    return cadastroEscolasService.findByName(_name);
+  }
+
 }
