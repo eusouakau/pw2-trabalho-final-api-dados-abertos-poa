b3dee838b53277db9b24289891d623c4d44e3f3f
Kauana Henriques <karoltvbl@gmail.com>
2022-07-06 00:45:30 -0300

    feat: (user) implementa service de servidores ativos na user

--- user-manager/src/main/java/ifrs/dev/services/ServidoresAtivosService.java
+++ user-manager/src/main/java/ifrs/dev/services/ServidoresAtivosService.java
@@ -1,20 +1,34 @@
 package ifrs.dev.services;
 
-// import java.util.List;
+import java.util.List;
 
-// import javax.ws.rs.GET;
-// import javax.ws.rs.Path;
-// import javax.ws.rs.Produces;
-// import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import io.vertx.core.json.JsonArray;
+
 @RegisterRestClient(baseUri = "http://localhost:3300/servidores-ativos")
+@Produces(MediaType.APPLICATION_JSON)
 public interface ServidoresAtivosService {
     
-    //  @GET
-    //  @Path("/servidores")
-    //  @Produces(MediaType.APPLICATION_JSON)
-    //  public List<Servidor> getAllServidores();
+  @GET
+  public JsonArray getAll();
+
+  @GET
+  public JsonArray getByOrigin(@PathParam("origem") String origem);
+
+  @GET
+  public String getTotalServidoresAtivos();
+
+  @GET
+  public double getSMEDBasicWage();
+
+  @GET
+  public String getTotalServidoresAtivosByOrigin(@PathParam("origem") String origem);
 
 }
