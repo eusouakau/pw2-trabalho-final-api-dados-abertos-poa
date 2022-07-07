96f5abf8a402341e2ec4a77cc52bae68073c15a3
amimenezes <mi.menezes.luiz@gmail.com>
2022-07-07 01:23:45 -0300

    FIX: corrige conflito

--- matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasService.java
+++ matriculas-escolas/src/main/java/ifrs/edu/MatriculasEscolasService.java
@@ -17,66 +17,34 @@ import io.vertx.core.json.JsonObject;
 @Produces(MediaType.APPLICATION_JSON)
 public interface MatriculasEscolasService {
 
-<<<<<<< HEAD
-  /*
-   * Id da API de Matrículas Escolas
-   * static String resource_id = "5b5bc272-6666-4d24-8ddd-138fea511809";
-   */
-  String resource_id = "5b5bc272-6666-4d24-8ddd-138fea511809";
+    /*
+     * Id da API de Matrículas Escolas
+     * static String resource_id = "5b5bc272-6666-4d24-8ddd-138fea511809";
+     */
+    String resource_id = "5b5bc272-6666-4d24-8ddd-138fea511809";
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public JsonObject getAll(
-      @QueryParam("resource_id") String resource_id);
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getAll(
+            @QueryParam("resource_id") String resource_id);
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public JsonObject getListarMatriculas(
-      @QueryParam("resource_id") String resource_id,
-      @QueryParam("fields") String codigo,
-      @QueryParam("fields") String nome,
-      @QueryParam("fields") String total);
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getListarMatriculas(
+            @QueryParam("resource_id") String resource_id,
+            @QueryParam("fields") String codigo,
+            @QueryParam("fields") String nome,
+            @QueryParam("fields") String total);
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public JsonObject getAtributoMatriculasEscolas(
-      @QueryParam("resource_id") String resource_id,
-      @QueryParam("filters") String filters);
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getAtributoMatriculasEscolas(
+            @QueryParam("resource_id") String resource_id,
+            @QueryParam("filters") String filters);
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public JsonObject getTotalMatriculas(
-      @QueryParam("resource_id") String resource_id,
-      @QueryParam("fields") String total);
-=======
-        /*
-         * Id da API de Matrículas Escolas
-         * static String resource_id = "5b5bc272-6666-4d24-8ddd-138fea511809";
-         */
-
-        @GET
-        @Produces(MediaType.APPLICATION_JSON)
-        public JsonObject getAll(
-                        @QueryParam("resource_id") String resource_id);
-
-        @GET
-        @Produces(MediaType.APPLICATION_JSON)
-        public JsonObject getListarMatriculas(
-                        @QueryParam("resource_id") String resource_id,
-                        @QueryParam("fields") String codigo,
-                        @QueryParam("fields") String nome,
-                        @QueryParam("fields") String total);
-
-        @GET
-        @Produces(MediaType.APPLICATION_JSON)
-        public JsonObject getAtributoMatriculasEscolas(
-                        @QueryParam("resource_id") String resource_id,
-                        @QueryParam("filters") String filters);
-
-        @GET
-        @Produces(MediaType.APPLICATION_JSON)
-        public JsonObject getTotalMatriculas(
-                        @QueryParam("resource_id") String resource_id,
-                        @QueryParam("fields") String total);
->>>>>>> 8ff1ccb344cf68c1238cf1bf093a9331f903b48b
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getTotalMatriculas(
+            @QueryParam("resource_id") String resource_id,
+            @QueryParam("fields") String total);
 }
